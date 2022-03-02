8dd5d73a043a5205e8b86ca8dc02b81ccde414b0
Ahmed Hamed <ahmedhrayyan@outlook.com>
2022-03-02 13:53:14 +0200

    Add project files.

---
+++ AlgorithmicToolbox/AlgorithmicToolbox.cpp
@@ -0,0 +1,11 @@
+// AlgorithmicToolbox.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "AlgorithmicToolbox.h"
+
+int main()
+{
+    int product = MaximumPairwiseProduct();
+    std::cout << product;
+}
---
+++ AlgorithmicToolbox/MaximumPairwiseProduct.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+#include "AlgorithmicToolbox.h";
+
+int MaximumPairwiseProduct()
+{
+	int n;
+	std::cin >> n;
+	const int size = n;
+	int max = std::numeric_limits<int>::min();
+	int secondMax = std::numeric_limits<int>::min();
+	for (int i = 0; i < n; i++)
+	{
+		int num;
+		std::cin >> num;
+		if (num > max)
+		{
+			secondMax = max;
+			max = num;
+		}
+		else if (num != max && num > secondMax) secondMax = num;
+	}
+
+	return max * secondMax;
+}
